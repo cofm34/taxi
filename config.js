9cdac041af2dbf80334aa592bc763b536d966f1e
cofm dubaii <96689200+cofm34@users.noreply.github.com>
2024-12-28 17:54:13 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const CONFIG = {
     FACEBOOK_ID: '100003998569785',  // ID Facebook của bạn
     LINE_ID: 'cofm34',         // ID Line của bạn
     ZALO_PHONE: '0976957811',        // Số điện thoại Zalo
-    PHONE_NUMBER: '+886976957811'    // Số điện thoại Đài Loan (+886 là mã quốc gia Đài Loan)
+    PHONE_NUMBER: '+886976957811'    // Số điện thoại Đài Loan (+886 là mã quốc gia Đài Loan) ok
 }; 
